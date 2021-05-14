83ddae3d9dc4e31fc6aec517404a8e1a8dabd7ca
Preeti12345678 <75837980+Preeti12345678@users.noreply.github.com>
2021-05-14 19:19:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,19 +5,20 @@ function setup(){
     canvas=createCanvas(700,600);
     canvas.position(400,200) ;
     video=createCapture(VIDEO);
-    video.position(600,300);
     video.hide();
+    textSize(20);
 }
 function draw(){
-    image(video, 0,0,500,400);
-    fill(128,128,128);
+    image(video, 100,30,400,500);
     stroke(0,0,0,0);
-    circle(110,50,70);
-    circle(40,120,70);
-    fill(142,69,133);
-    circle(80,100,70);
-    
+    fill(255,255,255);
+    rect(100, 30, 400, 20);
+    rect(100,30,20,500);
+    rect(100,500,420,80);
+    rect(500,30,20,500);
+    fill(0,0,0);
+    text('My polaroid picture!',225,550);
 }
 function take_snapshot(){
-    save('snapshot.png');
+    save('polaroidPicture.png');
 }
